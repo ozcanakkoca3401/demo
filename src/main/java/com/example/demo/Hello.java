72909858d8d68667ed48e780aec1aad49a7e2268
Özcan AKKOCA <ozcanakkoca3401@gmail.com>
2025-07-29 15:57:54 +0300

    version 2

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,13 +1,13 @@
-package com.example.demo;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Hello {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello World1";
-    }
-}
+//package com.example.demo;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class Hello {
+//
+//    @GetMapping("/")
+//    public String hello() {
+//        return "Hello World2";
+//    }
+//}
